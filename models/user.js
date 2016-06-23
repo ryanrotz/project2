583d9cb717479aa68a76946af91fd0d6594749ea
Ryan Rotz <ryanrotz@gmail.com>
2016-06-23 14:14:51 -0700

    create user with validations

---
+++ models/user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define('user', {
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email address'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 254],
+          msg: 'Password must be between 8 and 254 characters'
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [3, 254],
+          msg: 'Name must be between 1 and 254'
+        }
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return user;
+};
